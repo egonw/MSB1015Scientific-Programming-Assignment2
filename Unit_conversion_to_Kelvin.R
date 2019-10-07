efb83815c707edcf410c93cd567ca5423f982c60
prstolpe <pr.stolpe@gmail.com>
2019-10-07 15:31:55 +0200

    Added description to Unit_conversion_to_Kelvin.R
    Documentation for the function now includes a description of what it does and why it was created.

--- Unit_conversion_to_Kelvin.R
+++ Unit_conversion_to_Kelvin.R
@@ -1,3 +1,5 @@
+# This function detects, if a unit is not given in kelvin and converts it from either Celsius or Fahrenheit 
+# to kelvin. This makes it easier to compare boilingpoints to each other and build a regression model.
 Unit_conversion_to_Kelvin <- function (r) {
   # control for boilingpoint units: convert all to kelvin
   r$bp[r$bpUnitLabel == "degree Celsius"] <- r$bp[r$bpUnitLabel == "degree Celsius"] + 273.15
